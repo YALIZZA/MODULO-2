be72ead3114bd2ef5612d7b97832a22a07aa4ec6
YALIZZA <torres.elayne.10@gmail.com>
2025-02-10 20:59:54 -0500

    actulizar

---
+++ CLASE04/Factory1/Rectangulo.java
@@ -0,0 +1,11 @@
+package CLASE04.Factory1;
+
+public class Rectangulo implements Forma{
+
+    @Override
+    public void dibujar() {
+        System.out.println("Dibujando un Rectangulo");
+    }
+    
+
+}
