94d5962c544a46a17df3db1c2bb8762914a952a1
YALIZZA <torres.elayne.10@gmail.com>
2025-03-20 22:48:16 -0500

    no se que hice

--- RETO02/Reto02/Emergencia.java
+++ RETO02/Reto02/Emergencia.java
@@ -17,27 +17,50 @@ public class Emergencia{
  
 Scanner entrada = new Scanner(System.in);
 
- private int tipoEmergencia;
+ private String tipoEmergencia;
  private String ubicacion;
  private int nivelGravedad;
  private int tiempoRespuesta;
  boolean Atendido;
 
 //metodo constructor
-public Emergencia(int tipoEmergencia, String ubicacion, int nivelGravedad, int tiempoRespuesta) {
+public Emergencia(String tipoEmergencia, String ubicacion, int nivelGravedad, int tiempoRespuesta) {
     this.tipoEmergencia = tipoEmergencia;
     this.ubicacion = ubicacion;
     this.nivelGravedad = nivelGravedad;
     this.tiempoRespuesta = tiempoRespuesta;
 }
 
-public int getTipoEmergencia() {
-   // witch(tipoEmergencia){
-  //
-    return tipoEmergencia;
+/**
+ * This method ask the user to input the type of emergency and returns the name of the emergency selected.
+ * @return String with the name of the emergency selected.
+ */
+public String getTipoEmergencia() {
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Ingrese el tipo de Emergencia");
+    System.out.println("1. Incendio");
+    System.out.println("2. Robo");
+    System.out.println("3. Accidente");
+    System.out.println("4. Salir");
+    System.out.println("Ingrese una opcion: ");
+    int tipoEmergencia = entrada.nextInt();
+    String nombreEmergencia = null;
+    switch(tipoEmergencia){
+        
+        case 1: nombreEmergencia = "Incendio";
+            break;
+        case 2: nombreEmergencia = "Robo";
+            break;
+        case 3: nombreEmergencia = "Accidente";
+            break;
+        case 4: nombreEmergencia = "Salir";
+            System.out.println("4. A salido de la aplicacion de Emergencias.");
+    }
+   return nombreEmergencia;
+   
 }
 
-public void setTipoEmergencia(int tipoEmergencia) {
+public void setTipoEmergencia(String tipoEmergencia) {
     this.tipoEmergencia = tipoEmergencia;
 }
 
@@ -79,49 +102,6 @@ public void setAtendido(boolean atendido) {
         System.out.println("Nivel de Gravedad: " + nivelGravedad);
         System.out.println("Tiempo de Respuesta: " + tiempoRespuesta);
     }
-
-    private int menuEmergencia () {
-        System.out.println("¡Estas en la aplicacion de Emergencias!");
-        System.out.println("Ingrese el tipo de Emergencia");
-        System.out.println("1. Incendio");
-        System.out.println("2. Robo");
-        System.out.println("3. Accidente");
-        System.out.println("4. Salir");
-        System.out.println("Ingrese una opcion: ");
-        return entrada.nextInt();
-}
-
-private void asignarRecursos() {
-    final int VehiculosAmbulancia;
-    private int VehiculosPolicia;
-    protected int VehiculosBombero;
-    protected int RecursosPolicia;
-    protected int RecursosAmbulancia;
-    protected int RecursosBombero;
-
-    if (tipoEmergencia == 1) {
-        VehiculosAmbulancia = 2;
-        VehiculosPolicia = 2;
-        VehiculosBombero = 3;
-        RecursosPolicia = 100;
-        RecursosAmbulancia = 100;
-        RecursosBombero = 100;
-    } else if (tipoEmergencia == 2) {
-        VehiculosAmbulancia = 1;
-        VehiculosPolicia = 3;
-        VehiculosBombero = 0;
-        RecursosPolicia = 100;
-        RecursosAmbulancia = 100;
-        RecursosBombero = 100;
-    } else if (tipoEmergencia == 3) {
-        VehiculosAmbulancia = 2;
-        VehiculosPolicia = 2;
-        VehiculosBombero = 3;
-        RecursosPolicia = 100;
-        RecursosAmbulancia = 100;
-        RecursosBombero = 100;
-    }
-}
 
 
 }
